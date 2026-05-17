27215059fef302c5c4c52ccdfbc14d88bc7715fb
Agustinus Eko Kristiawan <167172206+krstwnn@users.noreply.github.com>
2026-05-17 18:15:10 +0700

    Update diy-part2.sh

--- config/immortalwrt_master/diy-part2.sh
+++ config/immortalwrt_master/diy-part2.sh
@@ -48,8 +48,8 @@ fi
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone -b main https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-# Add luci-app-qmodem
-git clone https://github.com/FUjr/luci-app-qmodem.git package/luci-app-qmodem
+# Add QModem and official modem feeds
+git clone https://github.com/FUjr/modem_feeds.git package/modem_feeds
 # Apply patches
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
